d43efb31afdfb35e6475da59bb678427b5615095
kimberly soler <kimberlysolja@gmail.com>
2023-10-24 18:28:39 -0500

    se crea carpeta source donde contiene la informacion de un ejemplo relacionado con la parte frontend para validar creación de api desde el backend. Contiene las carpetas index.html, index.js y style.css

---
+++ src/index.js
@@ -0,0 +1,19 @@
+document.getElementById("form-history").addEventListener("submit", async(event) => {
+    event.preventDefault(); //previene lo que hace por defecto el enviar un formulario
+    console.log(event);
+    const respuesta = await fetch("http://localhost:3000/api/informacion-historia", { //fetch permite realizar comunicación con el bakcend
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idea: event.target.children.idea.value,
+            project: event.target.children.project.value,
+            interest: event.target.children.interest.value,
+            knowledge: event.target.children.knowledge.value,
+            think: event.target.children.think.value,
+            information: event.target.children.information.value
+        })
+    });
+
+})
