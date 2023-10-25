1e89a15a9ee3332c10fc73648a3045b9ee3a9246
kimberly soler <kimberlysolja@gmail.com>
2023-10-24 21:38:29 -0500

    se crea una variable para guardar la información agregada en el metodo POST y se crea la función getHistoryInformation para responder en el método GET el objeto JSON y mostrarlos en la ruta definida para la API

--- controllers/autentication.controller.js
+++ controllers/autentication.controller.js
@@ -1,3 +1,7 @@
+const { response } = require("express");
+
+let historicalData = []; // se crea un array vacío para almacenar temporalmente información de los datos agregados
+
 async function historyInformation(request, response) {
     console.log(request.body);
     const idea = request.body.idea;//permite revisar cada una de las cosas que necesito
@@ -12,7 +16,36 @@ async function historyInformation(request, response) {
             message: "Los campos están incompletos"
         })
     }
+
+    historicalData.push({
+        idea,
+        project,
+        interest,
+        knowledge,
+        think,
+        information
+    }); // se implementa le método push con el fin de agregar la información en la variable historicalData
+    
+    response.json({
+        status: "Success",
+        message: "Información historica guardada exitosamente"
+    }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
 }
 
 
-module.exports.historyInformation = historyInformation;
+async function getHistoryInformation(request, response) {
+    try {
+        response.json({ historicalData })
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            status: "Error",
+            message: "Error al obtener información de historia"
+        })
+    }
+}; // esta función asincrona nos permite obtener los datos guardados temporalmente en la variables historicalData, cuando se actualice el servidor, esta información se perderá
+
+
+
+
+module.exports = { historyInformation, getHistoryInformation }; // permite exportación de funciones para implementación de estas en el archivo app.js
