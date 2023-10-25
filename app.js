47c664685fa101a1e5c35aac195441802f0365b0
kimberly soler <kimberlysolja@gmail.com>
2023-10-24 21:36:02 -0500

    se agrega metodo get para atrapar base de datos y visualizar en ruta definida para la API

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 //IMPORTACIÓN DE DEPENDENCIAS Y LIBRERIAS A USAR EN EL PROYECTO
 const express = require('express');
-const { request } = require('http');
 const path = require('path'); // inicialización de dependencias
 const app = express(); //variable donde va a escuchar la aplicación
-const { historyInformation } = require('./controllers/autentication.controller.js')
- //-------------------------------------------
+const { historyInformation, getHistoryInformation } = require('./controllers/autentication.controller.js')
+//-------------------------------------------
 
 // DEFINICIÓN DE PUERTO Y LLAMADO
 const PORT_DEFINED = process.env.PORT || 3000; //se crea el puerto donde queremos visualizar la información de forma local. 
 app.listen(PORT_DEFINED, () => {
-console.log(`El servidor está escuchando en http://localhost${PORT_DEFINED}...`);
+    console.log(`El servidor está escuchando en http://localhost${PORT_DEFINED}...`);
 }); // se llama el puerto para que este sea escuchado y se pueda visualizar desde express.js 
 //--------------------------------------------------------------------------------
 
@@ -21,16 +20,14 @@ app.use(express.json()); //permite leer formatos json en el framework express
 
 
 // RUTAS : acceso a las diferentes rutas que se van a crear en el proyecto
-app.get('/', (request, response) =>{
- response.sendFile(path.resolve(__dirname + '/src/index.html'));
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname + '/src/index.html'));
 });
 
 app.post('/api/informacion-historia', historyInformation);//permite agregar información requerida en nuesta api.
 
-app.get('/api/informacion-historia', (request, response) =>{
-    response.send(JSON.stringify(historyInformation));
-    console.log(JSON.stringify(historyInformation));
-});
+app.get('/api/informacion-historia', getHistoryInformation);// permite obtener la información con el método GET y visualizarla en la ruta respectiva
+
 
 //------------------------------------------------------
 
