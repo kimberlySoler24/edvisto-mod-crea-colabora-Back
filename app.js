25b8ba1e148f1b1a9dfda4703a8463b7dea13203
kimberly soler <kimberlysolja@gmail.com>
2023-10-27 10:28:15 -0500

    se crean dos metodos POST adicionales para crear la información de los formularios de la otras paginas adicionales y del mismo modo se crean las rutas respectivas de las páginas adicionales para visualizarlas en el puerto definido.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path'); // inicialización de dependencias
 const app = express(); //variable donde va a escuchar la aplicación
-const { historyInformation, getHistoryInformation } = require('./controllers/autentication.controller.js')
+const { historyInformation, historyInformationTwo, historyInformationThree, getHistoryInformation } = require('./controllers/autentication.controller.js')
 //-------------------------------------------
 
 // DEFINICIÓN DE PUERTO Y LLAMADO
@@ -13,22 +13,34 @@ app.listen(PORT_DEFINED, () => {
 //--------------------------------------------------------------------------------
 
 //VISUALIZACIÓN HTML: está parte del código me permite mostrar lo que se encuentra en estructurado en el archivo HTML
-
 //CONFIGURACION ESTÁTICA: permite scceder a todos los archivos situados en la carpeta src de forma estática, estático quiere decir que se sirven así tal cual como están sin ninguna modificación
 app.use(express.static(__dirname + '/src'));
 app.use(express.json()); //permite leer formatos json en el framework express
-
+app.use(express.static(__dirname + '/src/page-one'));
+app.use(express.json()); //permite leer formatos json en el framework express
+app.use(express.static(__dirname + '/src/page-two'));
+app.use(express.json()); //permite leer formatos json en el framework express
+app.use(express.static(__dirname + '/src/page-three'));
+app.use(express.json()); //permite leer formatos json en el framework express
 
 // RUTAS : acceso a las diferentes rutas que se van a crear en el proyecto
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname + '/src/index.html'));
+    response.sendFile(path.resolve(__dirname + '/index.html'));
 });
 
-app.post('/api/informacion-historia', historyInformation);//permite agregar información requerida en nuesta api.
+app.get('/index2', (request, response) => {
+    response.sendFile(path.resolve(__dirname + '/src/page-two/index2.html'));
+});
+
+app.get('/index3', (request, response) => {
+    response.sendFile(path.resolve(__dirname + '/src/page-three/index3.html'));
+});
+
+app.post('/api/informacion-pagina1', historyInformation);//permite agregar información requerida en nuesta api de la página 1.
+app.post('/api/informacion-pagina2', historyInformationTwo);//permite agregar información requerida en nuesta api de la página 2.
+app.post('/api/informacion-pagina3', historyInformationThree);//permite agregar información requerida en nuesta api de la página 3.
 
 app.get('/api/informacion-historia', getHistoryInformation);// permite obtener la información con el método GET y visualizarla en la ruta respectiva
-
-
 //------------------------------------------------------
 
 
