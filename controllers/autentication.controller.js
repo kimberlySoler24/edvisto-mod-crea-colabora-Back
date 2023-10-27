b534139db02dd04eb65f8a9f7551849919cc5da4
kimberly soler <kimberlysolja@gmail.com>
2023-10-27 10:30:33 -0500

    Se crean dos funciones adicionales para obtener la información respectiva de los formularios definidos en las demás páginas

--- controllers/autentication.controller.js
+++ controllers/autentication.controller.js
@@ -1,41 +1,56 @@
-const { response } = require("express");
-
 let historicalData = []; // se crea un array vacío para almacenar temporalmente información de los datos agregados
 
 async function historyInformation(request, response) {
     console.log(request.body);
-    const idea = request.body.idea;//permite revisar cada una de las cosas que necesito
+    const idea = request.body.idea;//permite obtener el valor de entrada de cada etiqueta HTML
     const project = request.body.project;
-    const interest = request.body.interest;
-    const knowledge = request.body.knowledge;
-    const think = request.body.think;
-    const information = request.body.information;
-    if (!idea || !project || !interest || !knowledge || !think || !information) {
+    if (!idea || !project ) {
         return response.status(400).send({
             status: "Error",
             message: "Los campos están incompletos"
         })
     }
 
-    historicalData.push({
-        idea,
-        project,
-        interest,
-        knowledge,
-        think,
-        information
-    }); // se implementa le método push con el fin de agregar la información en la variable historicalData
-    
-    response.json({
-        status: "Success",
-        message: "Información historica guardada exitosamente"
-    }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
+    historicalData.push({ idea, project }); // se implementa le método push con el fin de agregar la información en la variable historicalData
+    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
 }
 
+async function historyInformationTwo(request, response) {
+    console.log(request.body);
+    const interest = request.body.interest; //permite obtener el valor de entrada de cada etiqueta HTML
+    const knowledge = request.body.knowledge;
+    if (!interest || !knowledge) {
+        return response.status(400).send({
+            status: "Error",
+            message: "Los campos están incompletos"
+        })
+    }
+
+    historicalData.push({ interest, knowledge }); // se implementa le método push con el fin de agregar la información en la variable historicalData
+    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
+}
+
+async function historyInformationThree(request, response) {
+    console.log(request.body);
+    const think = request.body.think; //permite obtener el valor de entrada de cada etiqueta HTML
+    const information = request.body.information;
+    if (!think || !information) {
+        return response.status(400).send({
+            status: "Error",
+            message: "Los campos están incompletos"
+        })
+    }
+
+    historicalData.push({ think, information }); // se implementa le método push con el fin de agregar la información en la variable historicalData
+    
+    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
+
+    
+}
 
 async function getHistoryInformation(request, response) {
     try {
-        response.json({ historicalData })
+        response.json({ historicalData }) //genera la solicitud de respuesta de toda la información registrada en la variable de objeto historicalData
     } catch (error) {
         console.log(error);
         response.status(500).json({
@@ -46,6 +61,4 @@ async function getHistoryInformation(request, response) {
 }; // esta función asincrona nos permite obtener los datos guardados temporalmente en la variables historicalData, cuando se actualice el servidor, esta información se perderá
 
 
-
-
-module.exports = { historyInformation, getHistoryInformation }; // permite exportación de funciones para implementación de estas en el archivo app.js
+module.exports = { historyInformation, historyInformationTwo, historyInformationThree, getHistoryInformation }; // permite exportación de funciones para implementación de estas en el archivo app.js
