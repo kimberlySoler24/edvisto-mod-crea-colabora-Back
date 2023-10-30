dc94c2ca7fc6fcc3a81e23b4ff51a59543412f6f
kimberly soler <kimberlysolja@gmail.com>
2023-10-30 07:29:19 -0500

    Se agregan comentarios en los archivos javascript de cada pagina para tener claridad y llevar una idea de como se obtiene la información desde el HTML y se almacena en una URL.

--- src/page-one/index.js
+++ src/page-one/index.js
@@ -1,13 +1,16 @@
-document.getElementById("form-history").addEventListener("submit", async(event) => { //permite obtener los valores registrados en el formulario
-    event.preventDefault(); //previene lo que hace por defecto el enviar un formulario
-    console.log(event);
+//Código estructurado en lenguaje de programación JAVASCRIPT que utiliza el navegador web para interactuar con una página web.
 
-    const respuesta = await fetch("http://localhost:3000/api/informacion-pagina1", { //fetch permite realizar comunicación con el bakcend
-        method:"POST", //crea el formato json de lo que se quiere obtener desde los formularios con el metodo POST
-        headers:{
+
+document.getElementById("form-history").addEventListener("submit", async(event) => { //en esta sección se selecciona un elemento del HTML con el ID denominado form-history del formulario definido y agrega un escuchador de eventos, el cual estará atento a cuando ese formulario sea enviado 'submit'. Cueando eso sucede, se ejecutará la función proporcionada como segundo argumento, en este caso la función asíncrona con parámetro definido 'event'.
+    event.preventDefault(); // Por defecto, una página HTML se recarga cuando se efectúa el envío respectivo por medio del botón, esta línea de código evita que la página se recargue.
+    console.log(event); // muestra en la consola el objeto 'event'. Este objeto contiene información sobre el evento que ha icurrido, en este caso, el envío del formulario.
+
+    const respuesta = await fetch("http://localhost:3000/api/informacion-pagina1", { //fetch permite realizar comunicación con el bakcend, donde hacer una solicitud a la URL definida.
+        method:"POST", //Se específica que la solicitud es de tipo POST, que es uno de los métodos HTTP utilizados para enviar datos al servidor.
+        headers:{ // Se establece el encabezado de la solicitud HTTP para indicar que se está enviando datos en formato JSON.
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
+        body: JSON.stringify({ // Corresponde al cuerpo de la solicitud donde se definen los datos del objeto event que se enviarán al servidor. Estos datos los convierte en una cadena de texto JSON.
             idea: event.target.children.idea.value,
             project: event.target.children.project.value,
         })
@@ -16,3 +19,4 @@ document.getElementById("form-history").addEventListener("submit", async(event) 
 });
 
 
+//-------------------------------------------------------------------------------------
