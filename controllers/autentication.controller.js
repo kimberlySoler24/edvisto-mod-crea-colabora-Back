1e61835b4efa73f9963e9de8a3abf66b0fdefcf4
kimberly soler <kimberlysolja@gmail.com>
2023-11-07 11:04:31 -0500

    se adicionan controladores que van a permitir aplicar las solicitudes put para la aplicacion de la ai y delete cuando se requiera eliminar informacion

--- controllers/autentication.controller.js
+++ controllers/autentication.controller.js
@@ -1,64 +1,72 @@
-let historicalData = []; // se crea un array vacío para almacenar temporalmente información de los datos agregados
+const InformationSchema = require('../models/informationHistory.js');
 
 async function historyInformation(request, response) {
     console.log(request.body);
-    const idea = request.body.idea;//permite obtener el valor de entrada de cada etiqueta HTML
-    const project = request.body.project;
-    if (!idea || !project ) {
+    let informationSchema = new InformationSchema();
+
+    informationSchema.idea = request.body.idea; //permite obtener el valor de entrada de cada etiqueta HTML
+    informationSchema.project = request.body.project;
+    informationSchema.interest = request.body.interest; //permite obtener el valor de entrada de cada etiqueta HTML
+    informationSchema.knowledge = request.body.knowledge;
+    informationSchema.think = request.body.think; //permite obtener el valor de entrada de cada etiqueta HTML
+    informationSchema.information = request.body.information;
+
+
+    if (!informationSchema.idea  || !informationSchema.project || !informationSchema.interest  || !informationSchema.knowledge ||!informationSchema.think  || !informationSchema.information) {
         return response.status(400).send({
             status: "Error",
             message: "Los campos están incompletos"
         })
-    }
+    };
 
-    historicalData.push({ idea, project }); // se implementa le método push con el fin de agregar la información en la variable historicalData
-    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
+    informationSchema
+    .save()
+    .then((data) => response.status(200).json(data))
+    .catch((error) => response.status(500).json({message: `La información no se está guardando en la base de datos ${error}`}));
 }
 
-async function historyInformationTwo(request, response) {
-    console.log(request.body);
-    const interest = request.body.interest; //permite obtener el valor de entrada de cada etiqueta HTML
-    const knowledge = request.body.knowledge;
-    if (!interest || !knowledge) {
-        return response.status(400).send({
-            status: "Error",
-            message: "Los campos están incompletos"
-        })
-    }
 
-    historicalData.push({ interest, knowledge }); // se implementa le método push con el fin de agregar la información en la variable historicalData
-    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
-}
+ async function getHistoryInformation(request, response) {
 
-async function historyInformationThree(request, response) {
-    console.log(request.body);
-    const think = request.body.think; //permite obtener el valor de entrada de cada etiqueta HTML
-    const information = request.body.information;
-    if (!think || !information) {
-        return response.status(400).send({
-            status: "Error",
-            message: "Los campos están incompletos"
-        })
-    }
+    InformationSchema
+    .find()
+    .then((data) => response.status(200).json(data))
+    .catch((error) => response.status(500).json({message: `La información no se encuentra en la base de datos ${error}`}))
+  }; // esta función asincrona nos permite obtener los datos guardados temporalmente en la variables historicalData, cuando se actualice el servidor, esta información se perderá
 
-    historicalData.push({ think, information }); // se implementa le método push con el fin de agregar la información en la variable historicalData
-    
-    response.json({ historicalData }); // corresponde al resultado exitoso del método POST para almacenamiento de información en el objeto requerido.
 
-    
-}
+  async function getHistoryInformationById(request, response) {
+    const historyId= request.params.historyId 
+    InformationSchema
+    .findById(historyId)
+    .then((data) => response.status(200).json(data))
+    .catch((error) => response.status(500).json({message: `La información no se encuentra en la base de datos ${error}`}))
+  };
 
-async function getHistoryInformation(request, response) {
-    try {
-        response.json({ historicalData }) //genera la solicitud de respuesta de toda la información registrada en la variable de objeto historicalData
+  async function updateInformation(request, response) {
+    const historyId= request.params.historyId 
+    const {idea, project, interest, knowledge, think, information} = request.body;
+    InformationSchema
+    .updateOne({ _id: historyId }, { $set: { idea, project, interest, knowledge, think, information } } )
+    .then((data) => response.status(200).json(data))
+    .catch((error) => response.status(500).json({message: `La información no se encuentra en la base de datos ${error}`}))
+  };
+
+  async function deleteInformation(request, response) {
+    const historyId= request.params.historyId 
+
+    try{
+        const deletedInformation = await InformationSchema.findByIdAndDelete(historyId);
+        if(!deletedInformation){
+            return response.status(404).json({ message: 'No se encuentra en base de datos' })
+        };
+        response.status(200).json(deletedInformation);
     } catch (error) {
-        console.log(error);
-        response.status(500).json({
-            status: "Error",
-            message: "Error al obtener información de historia"
-        })
+        response.status(500).json({ message: `Error al eliminar la información: ${error}` })
     }
-}; // esta función asincrona nos permite obtener los datos guardados temporalmente en la variables historicalData, cuando se actualice el servidor, esta información se perderá
+    
+  };
 
 
-module.exports = { historyInformation, historyInformationTwo, historyInformationThree, getHistoryInformation }; // permite exportación de funciones para implementación de estas en el archivo app.js
+
+module.exports = { historyInformation, getHistoryInformation, getHistoryInformationById, updateInformation, deleteInformation}; // permite exportación de funciones para implementación de estas en el archivo app.js
