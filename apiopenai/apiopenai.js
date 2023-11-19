1483127b15ca4f4a0404e16132c08b2a425fb6bb
kimberly soler <kimberlysolja@gmail.com>
2023-11-19 06:12:03 -0500

    se define prompt y se ajusta temperatura de la api key openai

--- apiopenai/apiopenai.js
+++ apiopenai/apiopenai.js
@@ -13,12 +13,13 @@ async function responseOpenApiAI(request, response) {
     InformationSchema
     .findById(historyId)
     .then(async (data) => {
-        const mensajeUsuario = `Crear una historia basada en la información con ID ${historyId}:\n${JSON.stringify(data)}`;
+        const mensajeUsuario = `Actúa como un experto en editor de textos. Redacta un texto con un tono y estilo amigable y entendible para niños de 8 a 10 años.
+        Debe tener la siguiente  estructura :  estructura narrativa ARGUMENTATIVA [ introducción es un párrafo de 150 caracteres / desarrollo de argumentos son 2 párrafos de 400 caracteres cada uno / conclusión un párrafo de 250 caracteres], utiliza como base para la redacción lo siguiente información : ${historyId}:\n${JSON.stringify(data)}`;
         const chatCompletion = await openai.chat.completions.create({
             messages: [{ role: 'user', content: mensajeUsuario }],
             model: 'gpt-3.5-turbo',
-            temperature: 0.8,
-            max_tokens: 10
+            temperature: 0.1,
+            max_tokens: 1000
         });
 
         const responseOpenAI = chatCompletion.choices[0].message.content;
